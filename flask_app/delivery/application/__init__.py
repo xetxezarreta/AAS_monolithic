5c2b8e3905e530aa0bf87b044efebeac53e70c30
xetxezarreta <xabi1500@gmail.com>
2019-11-01 17:36:34 +0100

    commit

--- flask_app/delivery/application/__init__.py
+++ flask_app/delivery/application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine
 from .config import Config
+from .event_handler import Rabbit
 
 engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 Session = scoped_session(
@@ -15,6 +16,8 @@ Session = scoped_session(
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
+
+    Rabbit()
 
     with app.app_context():
         from . import routes
