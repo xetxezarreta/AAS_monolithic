8588660d03f452977eca3acdf8bdbe26747d9cb1
xetxezarreta <xabi1500@gmail.com>
2019-11-17 16:58:37 +0100

    commit

--- flask_app/client/application/routes.py
+++ flask_app/client/application/routes.py
@@ -53,10 +53,8 @@ def create_jwt():
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
         }
         print('55555555555555555', flush=True)
-        new_jwt = jwt.encode(payload, rsa_singleton.get_private_key(), algorithm='RS256')
-        print('666666666666666666', flush=True)
         response = {
-            'jwt': new_jwt
+            'jwt': jwt.encode(payload, rsa_singleton.get_private_key(), algorithm='RS256')
         }
         print('77777777777777777', flush=True)
     except:
@@ -78,24 +76,18 @@ def get_public_key():
 def unsupported_media_type_handler(e):
     return get_jsonified_error(e)
 
-
 @app.errorhandler(BadRequest)
 def bad_request_handler(e):
     return get_jsonified_error(e)
-
 
 @app.errorhandler(NotFound)
 def resource_not_found_handler(e):
     return get_jsonified_error(e)
 
-
 @app.errorhandler(InternalServerError)
 def server_error_handler(e):
     return get_jsonified_error(e)
 
-
 def get_jsonified_error(e):
     traceback.print_tb(e.__traceback__)
     return jsonify({"error_code":e.code, "error_message": e.description}), e.code
-
-
