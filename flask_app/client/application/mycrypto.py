b5fc14b87bbbfab0d1522849b4d1224fba5f85e0
Xabier Etxezarreta <xabi1500@gmail.com>
2019-12-12 17:35:45 +0100

    delete comments

--- flask_app/client/application/mycrypto.py
+++ flask_app/client/application/mycrypto.py
@@ -1,8 +1,8 @@
 from Crypto.PublicKey import RSA
 
 key = RSA.generate(2048)
-private_key = key.export_key()#.replace(b'RSA ', b'')
-public_key = key.publickey().export_key()#.replace(b'RSA ', b'')
+private_key = key.export_key()
+public_key = key.publickey().export_key()
 
 class rsa_singleton(object):
     @staticmethod
